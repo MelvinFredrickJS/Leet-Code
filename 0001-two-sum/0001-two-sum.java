1d2454692bb2a7982b4abc8254278a3f962e0183
MelvinFredrickJS <145590778+MelvinFredrickJS@users.noreply.github.com>
2025-01-09 11:28:39 +0530

    Time: 38 ms (44.72%), Space: 44.8 MB (80.84%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i+1;j<n;j++){
+                sum=nums[i]+nums[j];
+                if(sum==target){
+                    int []b={i,j};
+                    return b;
+                }
+            }
+        }
+        return new int[]{};
+        
+    }
+}
