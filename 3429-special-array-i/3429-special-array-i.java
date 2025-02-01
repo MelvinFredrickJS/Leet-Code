3113abe2b28ced134011b38b354d418e20e09932
MelvinFredrickJS <145590778+MelvinFredrickJS@users.noreply.github.com>
2025-02-01 18:41:09 +0530

    Time: 0 ms (100%), Space: 43.2 MB (72.64%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -1,19 +1,10 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        int flage=0;
         for(int i=0;i<nums.length-1;i++){
-            if(nums[i]%2==0){
-                if(nums[i+1]%2==0){
-                    flage=1;
-                }
-            }else if(nums[i+1]%2!=0){
-                flage=1;
+            if(nums[i]%2==nums[i+1]%2){
+                return false;
             }
         }
-        if(flage==0){
-            return true;
-        }else{
-            return false;
-        }
-    }
+        return true;
+}
 }
