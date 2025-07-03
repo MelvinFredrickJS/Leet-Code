7ed74307e858a497eeca41518cbc2beada72cfd7
MelvinFredrickJS <145590778+MelvinFredrickJS@users.noreply.github.com>
2025-07-03 20:02:03 +0530

    Time: 1 ms (99.58%), Space: 56.9 MB (70.23%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,8 +3,13 @@ class Solution {
         int max=Integer.MIN_VALUE;
         int sum=0;
         for(int i=0;i<nums.length;i++){
-            sum=Math.max(nums[i],sum+nums[i]);
-            max=Math.max(sum,max);
+            sum+=nums[i];
+            if(sum>max){
+                max=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
         }
         return max;
     }
