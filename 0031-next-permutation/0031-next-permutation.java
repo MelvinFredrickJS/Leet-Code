07e4a9ea24d96cc99a5aeb54431c0976ca5c3be7
MelvinFredrickJS <145590778+MelvinFredrickJS@users.noreply.github.com>
2025-07-08 20:43:44 +0530

    Time: 0 ms (100%), Space: 43.3 MB (36.75%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,13 +1,8 @@
 class Solution {
-    public void swap(int fn,int sn,int nums[]){
-        int temp=nums[fn];
-        nums[fn]=nums[sn];
-        nums[sn]=temp;
-
-    }
     public void nextPermutation(int[] nums) {
         int ind=-1;
-        for(int i=nums.length-2;i>=0;i--){
+        int len=nums.length;
+        for(int i=len-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 ind=i;
                 break;
@@ -15,30 +10,33 @@ class Solution {
         }
         if(ind==-1){
             int left=0;
-            int right=nums.length-1;
-            while(right>=left){
+            int right=len-1;
+            while(left<right){
                 int temp=nums[left];
                 nums[left]=nums[right];
                 nums[right]=temp;
                 left++;
                 right--;
             }
-            return;
-        }
-        for(int j=nums.length-1;j>ind;j--){
-            if(nums[ind]<nums[j]){
-                swap(ind,j,nums);
-                break;
+        }else if(ind!=-1){
+            for(int i=len-1;i>ind;i--){
+                if(nums[i]>nums[ind]){
+                    int temp=nums[i];
+                    nums[i]=nums[ind];
+                    nums[ind]=temp;
+                    break;
+                }
             }
-        }
-        int left=ind+1;
-        int right=nums.length-1;
-        while(right>=left){
-            int temp=nums[left];
-            nums[left]=nums[right];
-            nums[right]=temp;
-            left++;
-            right--;
+
+            int left=ind+1;
+            int right=len-1;
+            while(left<right){
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+                right--;
+            }
         }
         
     }
