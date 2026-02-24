2bcdceea987116df2fc633c49c71f05b7507b24f
MelvinFredrickJS <145590778+MelvinFredrickJS@users.noreply.github.com>
2026-02-24 08:50:59 +0530

    Time: 1 ms (99.90%) | Memory: 47 MB (45.08%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor=0;
-        for(int i=0;i<nums.length;i++){
-            xor=xor^nums[i];
+        int res=0;
+        for(int i:nums){
+            res^=i;
         }
-        return xor;
+        return res;
     }
 }
