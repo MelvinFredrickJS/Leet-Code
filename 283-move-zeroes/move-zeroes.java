671a89a8ab26af180df8e9e559477d83e89880db
MelvinFredrickJS <145590778+MelvinFredrickJS@users.noreply.github.com>
2026-03-25 06:24:37 +0530

    Time: 2 ms (90.65%) | Memory: 47.7 MB (84.38%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,22 +1,21 @@
 class Solution {
-    public void swap(int a, int n, int arr[]){
-        int temp=arr[a];
-        arr[a]=arr[n];
-        arr[n]=temp;
-
-    }
     public void moveZeroes(int[] nums) {
         int pointer=-1;
-        for(int k=0;k<nums.length;k++){
-            if(nums[k]==0){
-                pointer=k;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                pointer=i;
                 break;
             }
         }
-        if(pointer==-1) return;
+        if(pointer==-1){
+            
+            return;
+        }
         for(int j=pointer+1;j<nums.length;j++){
             if(nums[j]!=0){
-                swap(j,pointer,nums);
+                int temp=nums[j];
+                nums[j]=nums[pointer];
+                nums[pointer]=temp;
                 pointer++;
             }
         }
