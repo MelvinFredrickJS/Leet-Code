c55ab88d19fca53d3b1c4ee8f66774506d292cf3
MelvinFredrickJS <145590778+MelvinFredrickJS@users.noreply.github.com>
2026-03-25 07:10:59 +0530

    Time: 0 ms (100.00%) | Memory: 61 MB (90.65%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public void rev(int start,int stop, int arr[]){
-        while(start<=stop){
+        while(start<stop){
             int temp=arr[start];
             arr[start]=arr[stop];
             arr[stop]=temp;
@@ -9,13 +9,14 @@ class Solution {
         }
     }
     public void rotate(int[] nums, int k) {
-      int len=nums.length;
-      if(len==0) return ;
-      if(k>len){
-        k%=len;
-      }  
-      rev(0,len-1,nums);
-      rev(0,k-1,nums);
-      rev(k,len-1,nums);
+        int len=nums.length;
+        if(len==0)return;
+        if(k>len){
+            k%=len;
+        }
+        rev(0,nums.length-1,nums);
+        rev(0,k-1,nums);
+        rev(k,nums.length-1,nums);
+        
     }
 }
