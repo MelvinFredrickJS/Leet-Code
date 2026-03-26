9cb080735daca21f79ae538511536494f4c2c5d7
MelvinFredrickJS <145590778+MelvinFredrickJS@users.noreply.github.com>
2026-03-26 07:21:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (92.79%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,25 +1,22 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int fp=m-1;
-        int sp=nums2.length-1;
-        int place=nums1.length-1;
-        while(fp>=0 && sp>=0){
-        if(nums1[fp]>nums2[sp]){
-            nums1[place]=nums1[fp];
-            fp--;
-            place--;
-        }else{
-            nums1[place]=nums2[sp];
-            sp--;
-            place--;
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+            }else{
+                nums1[k]=nums2[j];
+                j--;
+            }
+            k--;
         }
+        while(j>=0){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
         }
-
-        while (sp >= 0) {
-        nums1[place] = nums2[sp];
-        sp--;
-        place--;
-    }
-
     }
 }
